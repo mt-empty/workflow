37e3dd4a9f41831e741b61b620b1aa0afb8fb0b4
MT <59728838+mt-empty@users.noreply.github.com>
2023-06-15 23:01:33 +1000

    fixed bash scripts

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-docker run --name workflow-redis -d redis redis-server --save 60 1 --loglevel warning
 POSTGRES_PASSWORD=$(grep -oP '(?<=POSTGRES_PASSWORD=).*' .env)
-docker run --name workflow-postgress -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" -p 5432:5432 -d postgres
+
+docker run --name workflow-redis -d redis redis-server --save 60 1 --loglevel warning
+
+docker run --name workflow-postgres -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" -p 5432:5432 -d postgres
--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker container stop workflow-redis workflow-postgres
-docker container rm workflow-redis workflow-postgres
+docker container stop workflow-redis workflow-postgres | true
+docker container rm workflow-redis workflow-postgres | true
