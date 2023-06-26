101f30a1af813247ff630bf5bcbfccebec6174e2
MT <59728838+mt-empty@users.noreply.github.com>
2023-06-26 19:08:33 +1000

    fixed cli subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -1,136 +1,158 @@
-// use anyhow::{Error as AnyError, Ok, Result};
-use clap::{Error, Parser, Subcommand};
-// use std::fs::File;
+use anyhow::{Error as AnyError, Ok, Result};
+use clap::{Parser, Subcommand};
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+use serde_yaml::from_str;
+use std::{
+    fs::{read_to_string, File},
+    io::Read,
+};
+
+use std::collections::BTreeMap;
 // use std::io::{BufRead, BufReader, Cursor, Read};
 // use std::path::{Path, PathBuf};
 // mod utils;
 // use utils::get_redis_con;
 
-const PAGES_BASE_DIR: &str = "";
+// const PAGES_BASE_DIR: &str = "";
 
+// #[clap(about = "A tool to manipulate  workflow tasks", author, version)]
 #[derive(Parser)]
-#[clap(arg_required_else_help(true))]
-#[clap(about = "A tool to manipulate  workflow tasks", author, version)]
 #[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
 struct Cli {
-    #[clap(subcommand)]
-    subcmd: SubCommand,
+    #[command(subcommand)]
+    command: Commands,
 }
 
-#[derive(Clap, Debug)]
-enum SubCommand {
-    #[clap(about = "Adds a task")]
-    Add(AddCommand),
-    #[clap(about = "Shows the status of a task")]
-    Show(ShowCommand),
-    #[clap(about = "Stops the task")]
-    Stop(StopCommand),
-    #[clap(about = "Continues the task")]
-    Continue(ContinueCommand),
-    #[clap(about = "Aborts the task")]
-    Abort(AbortCommand),
-    #[clap(about = "Lists all tasks")]
-    List(ListCommand),
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds workflow to the queue
+    Add {
+        file_path: String,
+    },
+    // Shows the status of a task
+    Show {
+        task_name: String,
+    },
+    // Stops the task
+    Stop {
+        task_name: String,
+    },
+    // Continues the task
+    Continue {
+        task_name: String,
+    },
+    // Aborts the task
+    Abort {
+        task_name: String,
+    },
+    // Lists all tasks
+    List,
 }
 
-#[derive(Clap, Debug)]
-struct AddCommand {
-    #[clap(index = 1, required = true)]
-    file: String,
-}
+pub fn cli() {
+    let cli = Cli::parse();
 
-#[derive(Clap, Debug)]
-struct ShowCommand {
-    #[clap(index = 1, required = true)]
-    task_name: String,
-}
-
-#[derive(Clap, Debug)]
-struct StopCommand {
-    #[clap(index = 1, required = true)]
-    task_name: String,
-}
-
-#[derive(Clap, Debug)]
-struct ContinueCommand {
-    #[clap(index = 1, required = true)]
-    task_name: String,
-}
-
-#[derive(Clap, Debug)]
-struct AbortCommand {
-    #[clap(index = 1, required = true)]
-    task_name: String,
-}
-
-#[derive(Clap, Debug)]
-struct ListCommand {}
-
-pub fn create_cli() -> MyCli {
-    MyCli::parse()
-}
-
-fn cli() {
-    let cli = cli::create_cli();
-
-    match cli.subcmd {
-        SubCommand::Add(add_cmd) => {
-            println!("Adding file: {}", add_cmd.file);
-            if let Err(e) = add(add_cmd.file) {
+    match &cli.command {
+        Commands::Add { file_path } => {
+            println!("Adding file: {}", file_path);
+            if let Err(e) = add(file_path.to_string()) {
                 println!("Failed to add file {}", e);
                 std::process::exit(1);
             }
-            // Add your logic for the 'add' subcommand here
+            // Add your logic for the 'add' Commands here
         }
-        SubCommand::Show(check_cmd) => {
-            println!("Showing task : {}", show_cmd.task_name);
-            // Add your logic for the 'check' subcommand here
+        Commands::Show { task_name } => {
+            println!("Showing task: {}", task_name);
+            // Add your logic for the 'show' Commands here
         }
-        SubCommand::Stop(stop_cmd) => {
-            println!("Stopping task : {}", stop_cmd.task_name);
-            // Add your logic for the 'check' subcommand here
+        Commands::Stop { task_name } => {
+            println!("Stopping task: {}", task_name);
+            // Add your logic for the 'stop' Commands here
         }
-        SubCommand::Continue(continue_cmd) => {
-            println!("Continuing task : {}", continue_cmd.task_name);
-            // Add your logic for the 'check' subcommand here
+        Commands::Continue { task_name } => {
+            println!("Continuing task: {}", task_name);
+            // Add your logic for the 'continue' Commands here
         }
-        SubCommand::Abort(abort_cmd) => {
-            println!("Deleting task : {}", abort_cmd.task_name);
-            // Add your logic for the 'check' subcommand here
+        Commands::Abort { task_name } => {
+            println!("Aborting task: {}", task_name);
+            // Add your logic for the 'abort' Commands here
         }
-        SubCommand::List(_) => {
-            println!("Showing status");
-            // Add your logic for the 'status' subcommand here
+        Commands::List => {
+            println!("Listing all tasks");
+            // Add your logic for the 'list' Commands here
         }
     }
 }
 
-fn add(file_path: String) -> Result<(), Error> {
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    name: String,
+    description: String,
+    data_sources: Vec<String>,
+}
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Root {
+    pub name: String,
+    pub description: String,
+    pub events: Events,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Events {
+    pub file_exists: FileExists,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FileExists {
+    pub path: String,
+    pub success: Success,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Success {
+    pub action: Action,
+    pub failure: Vec<String>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Action {
+    pub path: String,
+}
+
+fn add(file_path: String) -> Result<(), AnyError> {
     // open yaml file
-    let file = File::open(file_path)?;
+    // let mut map = BTreeMap::new();
+    let mut file = File::open(file_path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let config: Config = serde_yaml::from_str(&contents)?;
+    let config: Config = serde_yaml::from_reader(file)?;
     println!("{:?}", config);
 
-    let redis_result = get_redis_con();
-    if let Err(e) = redis_result {
-        println!("Failed to connect to redis {}", e);
-        println!("exiting...");
-        std::process::exit(1);
-    }
-    let mut redis_con = redis_result.unwrap();
-    let mock_task = Task {
-        uid: 1,
-        name: "name".to_string(),
-        description: "description".to_string(),
-        date: "date".to_string(),
-        time: "time".to_string(),
-        file: "./tests/tasks/create_foo.sh".to_string(),
-    };
-    let serialized_task = serialize(&mock_task).unwrap();
-    redis_con.rpush("test", serialized_task)?;
+    // let redis_result = get_redis_con();
+    // if let Err(e) = redis_result {
+    //     println!("Failed to connect to redis {}", e);
+    //     println!("exiting...");
+    //     std::process::exit(1);
+    // }
+    // let mut redis_con = redis_result.unwrap();
+    // let mock_task = Task {
+    //     uid: 1,
+    //     name: "name".to_string(),
+    //     description: "description".to_string(),
+    //     date: "date".to_string(),
+    //     time: "time".to_string(),
+    //     file: "./tests/tasks/create_foo.sh".to_string(),
+    // };
+    // let serialized_task = serialize(&mock_task).unwrap();
+    // redis_con.rpush("test", serialized_task)?;
 
     Ok(())
 }
